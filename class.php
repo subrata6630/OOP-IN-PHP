7d06d3cdf0fab60a2ce5067e48a2f11599211e02
Subrata Deb Nath <subrata6630@gmail.com>
2017-04-06 13:03:30 +0600

    Update class.php

--- class.php
+++ class.php
@@ -1,16 +1,5 @@
 <?php
-class car{
-	 public $Brand ="BMW";
-	 
-	 public function drive()
-	 {
-		 return " Object Oriented Programming  "
-	 }
-}
-
-
-$Car1 = new Car();
-$myData = $Car1 ->drive();
-
-echo $myData;
+class SimpleClass
+ {
+     }
 ?>
